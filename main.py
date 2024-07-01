8cdb9ec752cf24da962dcd3e89503dcb5af9c09d
fawazaudu <audu.fawaz.2@gmail.com>
2024-07-01 02:01:08 -0400

    commit

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ MAX_OPERAND = 15
 PROBLEMS = 10
 
 def generate_problem():
-    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    left_side = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right_side = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expression = str (left_side) + operator + str(right_side)
+    answer = eval(expression)
